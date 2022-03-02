426521cfa961a86ff62d119cf42caa3de49eb0e0
  <>
2022-03-02 12:13:04 +0200

    Viimeistelty irto-ohjelmien lisääminen dwm:stä tänne.

--- asenna.sh
+++ asenna.sh
@@ -1,2 +1,6 @@
 ln -f .emacs .xinitrc .bashrc ~/
-cp kuvakaappaus.sh /usr/local/bin/kuvakaappaus
+cp -f kuvakaappaus.sh /usr/local/bin/kuvakaappaus
+cp -f näyttö_vaihda.sh /usr/local/bin/näyttö_vaihda
+cp -f näyttö_lisää.sh /usr/local/bin/näyttö_lisää
+cp -f _hae_näytöt.sh /usr/local/bin
+cp -f nuku.sh /usr/local/bin/nuku
