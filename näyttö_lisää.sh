3a1617b8e109f1afcc794d687e0b4a67aff3ce8e
  <>
2022-03-10 01:52:16 +0200

    Näytön taustan laittaminen korjattu.

--- näyttö_lisää.sh
+++ näyttö_lisää.sh
@@ -3,5 +3,5 @@
 [ -z $sivunautto ] || [ -z $paanautto ] && exit
 [ $1 ] && suunta="$1" || suunta='--left-of'
 xrandr --output $sivunautto --auto $suunta $paanautto 
-[ $TAUSTA ] || TAUSTA='#000000'
-hsetroot -solid $TAUSTA
+[ $TAUSTA ] || TAUSTA='-solid #000000'
+hsetroot $TAUSTA
--- näyttö_vaihda.sh
+++ näyttö_vaihda.sh
@@ -3,5 +3,5 @@
 [ $sivunautto ] || exit
 [ $paanautto ]  &&  paanautto_pois="--output $paanautto --off"
 xrandr $paanautto_pois --output $sivunautto --auto --primary
-[ $TAUSTA ] || TAUSTA='#000000'
-hsetroot -solid $TAUSTA
+[ $TAUSTA ] || TAUSTA='-solid #000000'
+hsetroot $TAUSTA
